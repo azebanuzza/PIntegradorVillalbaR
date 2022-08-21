f61a3fd335230b7e9f37f317404ef6c871dd8d1e
azebanuzza <rodrigo_villalba@live.com.ar>
2022-08-21 12:35:25 -0300

    commit 21.8.22

--- PIntegradorVillalbaR/src/app/app.module.ts
+++ PIntegradorVillalbaR/src/app/app.module.ts
@@ -5,10 +5,42 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+=======
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
+  ],
+>>>>>>> Stashed changes
   providers: [],
   bootstrap: [AppComponent],
 })
